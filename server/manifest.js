4e1a93fed45244ca093dad36c35a532e024b728d
ConnerReeves <ConnerReeves@gmail.com>
2016-01-06 12:23:47 -0700

    manifest aquisition changes

--- server/manifest.js
+++ server/manifest.js
@@ -1,20 +1,17 @@
-var _ = require('lodash');
-var readDir = require('recursive-readdir');
-var path = require('path');
+const _ = require('lodash');
+const readDir = require('recursive-readdir');
+const path = require('path');
 
 const getPath = (file) => (path.extname(file) || '').replace('.', '').toUpperCase();
 const isImage = (file) => ['GIF', 'JPG', 'PNG', 'TIFF'].indexOf(getPath(file)) !== -1;
 const isVideo = (file) => ['MP4', 'OGG', 'WEBM'].indexOf(getPath(file)) !== -1;
 
 module.exports = {
-  getManifest: (dirs) => new Promise((resolve, reject) => {
-    Promise.all(dirs.map((dir) => new Promise((resolve, reject) => {
-      readDir(dir, (err, files) => resolve(files));
-    }))).then((files) => {
+  getManifest: (dir) => new Promise((resolve, reject) => {
+    readDir(dir, (err, files) => {
       const filteredFiles = _(files)
-        .flatten()
         .filter((file) => isImage(file) || isVideo(file))
-        .map((file) => ({ path: dirs.reduce((file, dir) => file.replace(dir, ''), file) }))
+        .map((file) => ({ path: file }))
         .value();
 
       resolve(filteredFiles);
