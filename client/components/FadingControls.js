ed3a32ceb5ef54e483fae612e9254b2dea100723
Conner Reeves <ConnerReeves@gmail.com>
2016-01-11 22:00:11 -0700

    focusing on controls added

--- client/components/FadingControls.js
+++ client/components/FadingControls.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
+import Focusable from 'Focusable';
 import PlaybackControlsContainer from 'containers/PlaybackControlsContainer';
 import FrequencyControlContainer from 'containers/FrequencyControlContainer';
 
 require('styles/fading-controls.scss');
 
 export default class FadingControls extends Component {
+  state = { focus: false };
+
   render() {
     return (
-      <div className="fading-controls" style={{ opacity: this.props.show ? 1 : 0 }}>
-        <PlaybackControlsContainer />
-        <FrequencyControlContainer />
+      <div className="fading-controls" style={{ opacity: this.props.show || this.state.focus ? 1 : 0 }}>
+        { this._getFocusable(<PlaybackControlsContainer />) }
+        { this._getFocusable(<FrequencyControlContainer />) }
       </div>
     );
+  }
+
+  _getFocusable(element) {
+    return (
+      <Focusable onBlur={ this._onBlur.bind(this) } onFocus={ this._onFocus.bind(this) }>
+        { element }
+      </Focusable>
+    );
+  }
+
+  _onFocus() {
+    this.setState({ focus: true });
+  }
+
+  _onBlur() {
+    this.setState({ focus: false });
   }
 }
---
+++ client/components/Focusable.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import PlaybackControlsContainer from 'containers/PlaybackControlsContainer';
+import FrequencyControlContainer from 'containers/FrequencyControlContainer';
+
+require('styles/fading-controls.scss');
+
+export default class Fucusable extends Component {
+  componentDidMount() {
+    const domNode = ReactDOM.findDOMNode(this);
+
+    domNode.addEventListener('mouseenter', this.props.onFocus);
+    domNode.addEventListener('mouseleave', this.props.onBlur);
+  }
+
+  render() {
+    return (
+      <div>{ this.props.children }</div>
+    );
+  }
+}
